33f661f33f1e2434ac28a0701fdec2d2d6eee787
Mufiidz <melody+dev@slmail.me>
2024-06-22 12:49:10 +0700

    ui: change color item notification

--- lib/screens/notif/item_notification.dart
+++ lib/screens/notif/item_notification.dart
@@ -14,35 +14,58 @@ class ItemNotification extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final model.Notification(:message, :createdAt) = notification;
+    final model.Notification(:message, :createdAt, :isRead) = notification;
     return Container(
       width: context.mediaSize.width,
       padding: const EdgeInsets.only(bottom: 8),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(6),
         child: InkWell(
-          onTap: onClick,
+          onTap: isRead == false ? onClick : null,
           borderRadius: BorderRadius.circular(6),
           child: Ink(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(6),
-              color: War9aColors.greyF2,
+              border: Border.all(color: War9aColors.greyC4.withOpacity(0.15)),
+              color: isRead
+                  ? Colors.white.withOpacity(0.20)
+                  : War9aColors.greyC4.withOpacity(0.15),
             ),
             child: Padding(
               padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    message,
-                    style: War9aTextstyle.normal,
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0, top: 6.0),
+                    child: Container(
+                      width: 8,
+                      height: 8,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: !isRead
+                              ? War9aColors.primaryColor
+                              : Colors.white.withOpacity(0.20)),
+                    ),
                   ),
-                  const SpacerWidget(8),
-                  Text(
-                    createdAt?.formatWithoutTime ?? '-',
-                    style: War9aTextstyle.normal.copyWith(fontSize: 11),
-                  )
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          message,
+                          style: War9aTextstyle.normal,
+                        ),
+                        const SpacerWidget(8),
+                        Text(
+                          createdAt?.formatWithoutTime ?? '-',
+                          style: War9aTextstyle.normal.copyWith(fontSize: 11),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
